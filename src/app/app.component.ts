a241ff6d017419222c0fa5b4d09c8cef5713d2df
Mikhail <vms2108@mail.ru>
2018-12-14 16:23:10 +0300

    first changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  text = '';
+
+  mousedown() {
+    this.text = 'mousedown';
+  }
+  mouseup() {
+    this.text = '';
+  }
 }
