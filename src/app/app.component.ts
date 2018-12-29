a7bbf0e982f51aff4c245406d84db643778b684f
mikhail <vms2108@mail.ru>
2018-12-29 15:00:50 +0300

    somethink works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   text = '';
+  move = false;
+  info = {
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+  };
+  elem: Element;
 
   mousedown(event) {
+    // const elem = document.querySelector('.block');
+    this.elem = event.target;
+    this.elem.style.position = 'relative';
+    this.elem.style.cursor = 'pointer';
+    this.info = this.elem.getBoundingClientRect();
+    // console.log(elem);
     this.text = event.clientX;
+    this.move = true;
+   // event.target.getBoundingClientRect().top = 100;
   }
-  mouseup() {
-    this.text = '';
+  mouseup(event) {
+    this.move = false;
+    event.target.style.top = this.elem.style.top;
+    event.target.style.left = this.elem.style.left;
   }
 
-  mouseout() {
-    this.text = '';
+  mouseout(event) {
+    this.move = false;
   }
 
   mousemove(event) {
-    this.text = event.clientX + ':' + event.clientY + '///' + event.pageX + ':' + event.pageY;
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  clickout(event) {
+    if (this.move) {
+      this.text = 'I am here';
+      this.elem.style.top =  event.clientY - this.info.top + 'px';
+      this.elem.style.left =  event.clientX - this.info.left + 'px';
+    }
   }
 }
